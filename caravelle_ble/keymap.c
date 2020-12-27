842efee75c1c8dca184abd75553eac243bc6dbfc
yaboojp <yaboo.ja@gmail.com>
2020-12-28 01:18:33 +0900

    add caravelle_ble

---
+++ caravelle_ble/keymap.c
@@ -0,0 +1,229 @@
+/*
+Copyright 2019 Sekigon
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+#include "app_ble_func.h"
+#include <stdio.h>
+
+enum custom_keycodes {
+    AD_WO_L = SAFE_RANGE, /* Start advertising without whitelist  */
+    BLE_DIS,              /* Disable BLE HID sending              */
+    BLE_EN,               /* Enable BLE HID sending               */
+    USB_DIS,              /* Disable USB HID sending              */
+    USB_EN,               /* Enable USB HID sending               */
+    DELBNDS,              /* Delete all bonding                   */
+    ADV_ID0,              /* Start advertising to PeerID 0        */
+    ADV_ID1,              /* Start advertising to PeerID 1        */
+    ADV_ID2,              /* Start advertising to PeerID 2        */
+    ADV_ID3,              /* Start advertising to PeerID 3        */
+    ADV_ID4,              /* Start advertising to PeerID 4        */
+    BATT_LV,              /* Display battery level in milli volts */
+    DEL_ID0,              /* Delete bonding of PeerID 0           */
+    DEL_ID1,              /* Delete bonding of PeerID 1           */
+    DEL_ID2,              /* Delete bonding of PeerID 2           */
+    DEL_ID3,              /* Delete bonding of PeerID 3           */
+    DEL_ID4,              /* Delete bonding of PeerID 4           */
+    ENT_DFU,              /* Start bootloader                     */
+    ENT_SLP,              /* Deep sleep mode                      */
+    LOWER,                /* Layer  keycode                       */
+    RAISE,                /* Layer  keycode                       */
+};
+
+
+extern keymap_config_t keymap_config;
+
+enum {
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+};
+
+// Layer related keycodes
+#define ADJUST  MO(_ADJUST)
+
+// Fillers to make layering more clear
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
+#define KC_LLANG1 LT(_LOWER, KC_LANG1)
+#define KC_LLANG2 LT(_LOWER, KC_LANG2)
+#define KC_RLANG1 LT(_RAISE, KC_LANG1)
+#define KC_RLANG2 LT(_RAISE, KC_LANG2)
+
+#define KC_QVOLU LALT(LSFT(KC__VOLUP))
+#define KC_QVOLD LALT(LSFT(KC__VOLDOWN))
+
+#define KC_LMOVE LGUI(LSFT(KC_LBRC))
+#define KC_RMOVE LGUI(LSFT(KC_RBRC))
+
+#define KC_CTLGRV LCTL(KC_GRV)
+
+#define KC_GUI_1 LGUI(KC_1)
+#define KC_GUI_2 LGUI(KC_2)
+#define KC_GUI_3 LGUI(KC_3)
+#define KC_GUI_4 LGUI(KC_4)
+#define KC_GUI_5 LGUI(KC_5)
+#define KC_GUI_6 LGUI(KC_6)
+#define KC_GUI_7 LGUI(KC_7)
+#define KC_GUI_8 LGUI(KC_8)
+
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT(
+ //+--------+--------+--------+--------+--------+--------+                        +--------+--------+--------+--------+--------+--------+
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,  \
+ //|--------+--------+--------+--------+--------+--------+--------+      +--------+--------+--------+--------+--------+--------+--------|
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN,        KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+ //|--------+--------+--------+--------+--------+--------+--------|      |--------+--------+--------+--------+--------+--------+--------|
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,        KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
+ //|--------+--------+--------+--------+--------+--------+--------|      |--------+--------+--------+--------+--------+--------+--------|
+                               ADJUST,  KC_LALT, KC_LGUI, KC_LLANG2,      KC_RLANG1,KC_SPC, KC_ESC,  KC_BSPC \
+ //                           +--------+--------+--------+--------+      +--------+--------+--------+--------+
+  ),
+
+  [_LOWER] = LAYOUT(
+ //+--------+--------+--------+--------+--------+--------+                        +--------+--------+--------+--------+--------+--------+
+    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
+ //|--------+--------+--------+--------+--------+--------+--------+      +--------+--------+--------+--------+--------+--------+--------|
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,        _______,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+ //|--------+--------+--------+--------+--------+--------+--------|      |--------+--------+--------+--------+--------+--------+--------|
+    _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, \
+ //|--------+--------+--------+--------+--------+--------+--------|      |--------+--------+--------+--------+--------+--------+--------|
+                               _______, _______, _______, _______,        _______, _______, _______, _______ \
+ //                           +--------+--------+--------+--------+      +--------+--------+--------+--------+
+  ),
+
+  [_RAISE] = LAYOUT(
+ //+--------+--------+--------+--------+--------+--------+                        +--------+--------+--------+--------+--------+--------+
+    _______, KC_GRV,  KC_BSLS, KC_EQL,  KC_PIPE, KC_PLUS,                          XXXXXXX, KC_LMOVE,KC_CTLGRV,KC_RMOVE,XXXXXXX,_______, \
+ //|--------+--------+--------+--------+--------+--------+--------+      +--------+--------+--------+--------+--------+--------+--------|
+    _______, KC_TILD, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, _______,        XXXXXXX, KC_PGUP, KC_HOME, KC_UP,   KC_END,  XXXXXXX, _______, \
+ //|--------+--------+--------+--------+--------+--------+--------|      |--------+--------+--------+--------+--------+--------+--------|
+    _______, _______, _______, _______, _______, _______, _______,        XXXXXXX, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, _______, \
+ //|--------+--------+--------+--------+--------+--------+--------|      |--------+--------+--------+--------+--------+--------+--------|
+                               _______, _______, _______, _______,        _______, _______, _______, _______ \
+ //                           +--------+--------+--------+--------+      +--------+--------+--------+--------+
+  ),
+
+  [_ADJUST] = LAYOUT ( \
+ //+--------+--------+--------+--------+--------+--------+                        +--------+--------+--------+--------+--------+--------+
+    _______, AD_WO_L, ADV_ID1, ADV_ID2, ADV_ID3, ADV_ID4,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+ //|--------+--------+--------+--------+--------+--------+--------+      +--------+--------+--------+--------+--------+--------+--------|
+    _______, DELBNDS, DEL_ID1, DEL_ID2, DEL_ID3, DEL_ID4, XXXXXXX,        XXXXXXX, XXXXXXX, KC__MUTE,KC_QVOLD,KC_QVOLU,XXXXXXX, XXXXXXX, \
+ //|--------+--------+--------+--------+--------+--------+--------|      |--------+--------+--------+--------+--------+--------+--------|
+    _______, BATT_LV, ENT_SLP, ENT_DFU, RESET,   XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, KC_MRWD, KC_MPLY, KC_MFFD, XXXXXXX, XXXXXXX, \
+ //|--------+--------+--------+--------+--------+--------+--------|      |--------+--------+--------+--------+--------+--------+--------|
+                               _______, _______, _______, _______,        _______, _______, _______, _______ \
+ //                           +--------+--------+--------+--------+      +--------+--------+--------+--------+
+  )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  char str[16];
+  switch (keycode) {
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+  }
+  if (record->event.pressed) {
+    switch (keycode) {
+    case DELBNDS:
+      delete_bonds();
+      return false;
+    case AD_WO_L:
+      restart_advertising_wo_whitelist();
+      return false;
+    case USB_EN:
+      set_usb_enabled(true);
+      return false;
+      break;
+    case USB_DIS:
+      set_usb_enabled(false);
+      return false;
+      break;
+    case BLE_EN:
+      set_ble_enabled(true);
+      return false;
+      break;
+    case BLE_DIS:
+      set_ble_enabled(false);
+      return false;
+      break;
+    case ADV_ID0:
+      restart_advertising_id(0);
+      return false;
+    case ADV_ID1:
+      restart_advertising_id(1);
+      return false;
+    case ADV_ID2:
+      restart_advertising_id(2);
+      return false;
+    case ADV_ID3:
+      restart_advertising_id(3);
+      return false;
+    case ADV_ID4:
+      restart_advertising_id(4);
+      return false;
+    case DEL_ID0:
+      delete_bond_id(0);
+      return false;
+    case DEL_ID1:
+      delete_bond_id(1);
+      return false;
+    case DEL_ID2:
+      delete_bond_id(2);
+      return false;
+    case DEL_ID3:
+      delete_bond_id(3);
+      return false;
+    case BATT_LV:
+      sprintf(str, "%4dmV", get_vcc());
+      send_string(str);
+      return false;
+    case ENT_DFU:
+      bootloader_jump();
+      return false;
+    }
+  }
+  else if (!record->event.pressed) {
+    switch (keycode) {
+    case ENT_SLP:
+      sleep_mode_enter();
+      return false;
+    }
+
+  }
+  return true;
+}
+;
